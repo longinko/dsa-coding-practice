423ac53c2755d8212a3024b05b589b78ff8ce475
Longin <longin.koziolkiewicz@gmail.com>
2026-03-27 09:14:40 +0100

    RemoveElement

--- java/src/problems/array/RemoveElement.java
+++ java/src/problems/array/RemoveElement.java
@@ -2,6 +2,7 @@ package problems.array;
 
 public class RemoveElement {
 
+    //You are given an integer array nums and an integer val. Your task is to remove all occurrences of val from nums in-place.
     public static void main(String[] args) {
         int[] nums = {1, 1, 2, 3, 4, 5};
         System.out.println(removeElement(nums, 1));
